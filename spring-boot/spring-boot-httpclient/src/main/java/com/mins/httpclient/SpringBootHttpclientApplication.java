ef9d5f97a87c32f73994c558106f639442961502
zhengjunming <1594998836@qq.com>
2018-05-16 23:07:54 +0800

    add spring-boot-httpclient

---
+++ spring-boot/spring-boot-httpclient/src/main/java/com/mins/httpclient/SpringBootHttpclientApplication.java
@@ -0,0 +1,15 @@
+package com.mins.httpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小铭
+ */
+@SpringBootApplication
+public class SpringBootHttpclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHttpclientApplication.class, args);
+    }
+}
